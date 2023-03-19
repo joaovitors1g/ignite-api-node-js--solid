c327528b52d90228ec1b0e71c8bdcaeac93ebf34
joaovitors1g <joao_vitor9846@hotmail.com>
2023-03-19 11:57:51 -0300

    feat: add global error handling

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,25 @@
 import fastify from 'fastify'
+import { ZodError } from 'zod'
+import { env } from './env'
 import { appRoutes } from './routes'
 
 export const app = fastify()
 
 app.register(appRoutes)
+
+app.setErrorHandler((error, request, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: 'Validation failed',
+      errors: error.format(),
+    })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  }
+
+  return reply.status(500).send({
+    message: 'Internal server error',
+  })
+})
