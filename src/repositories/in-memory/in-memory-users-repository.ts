ccb7515635e9c23259a3dde737644530230ec6ee
joaovitors1g <joao_vitor9846@hotmail.com>
2023-03-20 22:00:32 -0300

    refactor: change test id to uuids using node:crypto

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -1,4 +1,5 @@
 import { User, Prisma } from '@prisma/client'
+import { randomUUID } from 'node:crypto'
 import { IUsersRepository } from '../users-repository'
 
 export class InMemoryUsersRepository implements IUsersRepository {
@@ -18,7 +19,7 @@ export class InMemoryUsersRepository implements IUsersRepository {
 
   async create(data: Prisma.UserCreateInput): Promise<User> {
     const user = {
-      id: 'user-1',
+      id: randomUUID(),
       name: data.name,
       email: data.email,
       password_hash: data.password_hash,
