c43e019cf309f8938c2f238c4b07b9efd451dcaa
joaovitors1g <joao_vitor9846@hotmail.com>
2023-03-20 22:21:50 -0300

    feat: add implementation for find check in by user id and date

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -5,6 +5,21 @@ import { ICheckInsRepository } from '../check-ins-repository'
 export class InMemoryCheckInsRepository implements ICheckInsRepository {
   private checkIns: CheckIn[] = []
 
+  async findByUserIdOnDate(
+    userId: string,
+    date: Date,
+  ): Promise<CheckIn | null> {
+    const checkIn = this.checkIns.find(
+      (checkIn) =>
+        checkIn.user_id === userId &&
+        checkIn.created_at.getDate() === date.getDate() &&
+        checkIn.created_at.getMonth() === date.getMonth() &&
+        checkIn.created_at.getFullYear() === date.getFullYear(),
+    )
+
+    return checkIn ?? null
+  }
+
   async create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn> {
     const checkIn = {
       id: randomUUID(),
