ee9e613874f5983e0cf6bbd647d748b44e735a22
joaovitors1g <joao_vitor9846@hotmail.com>
2023-03-22 22:06:45 -0300

    feat: add create gym to gyms repository

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,4 +1,5 @@
-import { Gym } from '@prisma/client'
+import { Gym, Prisma } from '@prisma/client'
+import { randomUUID } from 'crypto'
 import { IGymsRepository } from '../gyms-repository'
 
 export class InMemoryGymsRepository implements IGymsRepository {
@@ -8,5 +9,21 @@ export class InMemoryGymsRepository implements IGymsRepository {
     const gym = this.gyms.find((gym) => gym.id === id)
 
     return gym ?? null
+  }
+
+  async create(data: Prisma.GymCreateInput): Promise<Gym> {
+    const gym = {
+      id: data.id ?? randomUUID(),
+      name: data.name,
+      description: data.description ?? null,
+      phone: data.phone ?? null,
+      latitude: new Prisma.Decimal(data.latitude.toString()),
+      longitude: new Prisma.Decimal(data.longitude.toString()),
+      created_at: new Date(),
+    }
+
+    this.gyms.push(gym)
+
+    return gym
   }
 }
