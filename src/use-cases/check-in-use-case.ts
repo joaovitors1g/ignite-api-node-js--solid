da77f98dcefee76a72ef00d67480d8c2088ddcf0
joaovitors1g <joao_vitor9846@hotmail.com>
2023-03-22 21:46:14 -0300

    feat: add distance validation before check in

--- src/use-cases/check-in-use-case.ts
+++ src/use-cases/check-in-use-case.ts
@@ -1,9 +1,14 @@
 import { ICheckInsRepository } from '@/repositories/check-ins-repository'
+import { IGymsRepository } from '@/repositories/gyms-repository'
+import { getDistanceBetweenCoordinates } from '@/utils/get-distance-between-coordinates'
 import { CheckIn } from '@prisma/client'
+import { ResourceNotFoundError } from './errors/resource-not-found'
 
 interface ICheckInRequest {
   userId: string
   gymId: string
+  userLatitude: number
+  userLongitude: number
 }
 
 interface ICheckInResponse {
@@ -11,9 +16,40 @@ interface ICheckInResponse {
 }
 
 export class CheckinUseCase {
-  constructor(private checkInsRepository: ICheckInsRepository) {}
+  constructor(
+    private checkInsRepository: ICheckInsRepository,
+    private gymsRepository: IGymsRepository,
+  ) {}
 
-  async execute({ userId, gymId }: ICheckInRequest): Promise<ICheckInResponse> {
+  async execute({
+    userId,
+    gymId,
+    userLatitude,
+    userLongitude,
+  }: ICheckInRequest): Promise<ICheckInResponse> {
+    const gym = await this.gymsRepository.findById(gymId)
+
+    if (!gym) {
+      throw new ResourceNotFoundError()
+    }
+
+    const distance = getDistanceBetweenCoordinates(
+      {
+        latitude: userLatitude,
+        longitude: userLongitude,
+      },
+      {
+        latitude: gym.latitude.toNumber(),
+        longitude: gym.longitude.toNumber(),
+      },
+    )
+
+    const MAX_DISTANCE_IN_KM = 0.1
+
+    if (distance > MAX_DISTANCE_IN_KM) {
+      throw new Error('User is too far from gym')
+    }
+
     const checkInOnNowDate = await this.checkInsRepository.findByUserIdOnDate(
       userId,
       new Date(),
