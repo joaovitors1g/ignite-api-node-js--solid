b17eab9a65df5e71a9faac891d6d2bc8b9a83b71
joaovitors1g <joao_vitor9846@hotmail.com>
2023-03-22 22:07:51 -0300

    test: add tests for create gym use case

---
+++ src/use-cases/create-gym-use-case.spec.ts
@@ -0,0 +1,26 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { CreateGymUseCase } from './create-gym-use-case'
+
+describe('Create Gym Use Case', () => {
+  let gymsRepository: InMemoryGymsRepository
+  let sut: CreateGymUseCase
+
+  beforeEach(() => {
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new CreateGymUseCase(gymsRepository)
+  })
+
+  it('should be able to register', async () => {
+    const { gym } = await sut.execute({
+      name: 'Good Gym',
+      phone: null,
+      description: null,
+      latitude: -25.4329826,
+      longitude: -49.2679787,
+    })
+
+    expect(gym.id).toEqual(expect.any(String))
+  })
+})
