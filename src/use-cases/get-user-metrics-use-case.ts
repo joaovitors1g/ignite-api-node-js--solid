575a160758b8f1f6143c69f302ab3971d64a1789
joaovitors1g <joao_vitor9846@hotmail.com>
2023-03-23 21:55:43 -0300

    feat: add get user check ins count use case

---
+++ src/use-cases/get-user-metrics-use-case.ts
@@ -0,0 +1,23 @@
+import { ICheckInsRepository } from '@/repositories/check-ins-repository'
+
+interface IGetUserMetricsRequest {
+  userId: string
+}
+
+interface IGetUserMetricsResponse {
+  checkInsCount: number
+}
+
+export class GetUserMetricsUseCase {
+  constructor(private checkInsRepository: ICheckInsRepository) {}
+
+  async execute({
+    userId,
+  }: IGetUserMetricsRequest): Promise<IGetUserMetricsResponse> {
+    const checkInsCount = await this.checkInsRepository.countByUserId(userId)
+
+    return {
+      checkInsCount,
+    }
+  }
+}
