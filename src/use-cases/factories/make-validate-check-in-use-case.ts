96af8f81fc30100e1768a8335c7d342048581232
joaovitors1g <joao_vitor9846@hotmail.com>
2023-03-26 17:23:55 -0300

    feat: add factory for validate check-in use case

---
+++ src/use-cases/factories/make-validate-check-in-use-case.ts
@@ -0,0 +1,10 @@
+import { PrismaCheckInsRepository } from '@/repositories/prisma/prisma-check-ins-repository'
+import { ValidateCheckInUseCase } from '../validate-check-in-use-case'
+
+export function makeValidateCheckInUseCase() {
+  const checkInRepository = new PrismaCheckInsRepository()
+
+  const validateCheckInUseCase = new ValidateCheckInUseCase(checkInRepository)
+
+  return validateCheckInUseCase
+}
