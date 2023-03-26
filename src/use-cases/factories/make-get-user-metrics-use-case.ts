85b9cd8b7d50bbb38f0190c86894a6f2fb361f27
joaovitors1g <joao_vitor9846@hotmail.com>
2023-03-26 17:23:32 -0300

    feat: add factory for get user checkins metrics use case

---
+++ src/use-cases/factories/make-get-user-metrics-use-case.ts
@@ -0,0 +1,10 @@
+import { PrismaCheckInsRepository } from '@/repositories/prisma/prisma-check-ins-repository'
+import { GetUserMetricsUseCase } from '../get-user-metrics-use-case'
+
+export function makeGetUserMetricsUseCase() {
+  const checkInRepository = new PrismaCheckInsRepository()
+
+  const getUserMetricsUseCase = new GetUserMetricsUseCase(checkInRepository)
+
+  return getUserMetricsUseCase
+}
