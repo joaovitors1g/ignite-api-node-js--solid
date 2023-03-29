8ebc638930f1405d7c8c3e919b233024d2b06be8
joaovitors1g <joao_vitor9846@hotmail.com>
2023-03-28 22:38:07 -0300

    feat: add user profile route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,19 @@
 import { AuthController } from '@/controllers/authController'
+import { ProfileController } from '@/controllers/profileController'
 import { RegisterController } from '@/controllers/registerController'
+import { verifyJwt } from '@/middlewares/verify-jwt'
 import { FastifyInstance } from 'fastify'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', RegisterController.register)
   app.post('/sessions', AuthController.authenticate)
+
+  /** Authenticated Routes */
+  app.get(
+    '/me',
+    {
+      onRequest: verifyJwt,
+    },
+    ProfileController.get,
+  )
 }
