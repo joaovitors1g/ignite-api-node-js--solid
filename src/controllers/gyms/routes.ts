33cafe3312cb781622cd82f5f10f5e5dc5bd8628
joaovitors1g <joao_vitor9846@hotmail.com>
2023-04-23 18:17:00 -0300

    feat: add min role for gym creation

--- src/controllers/gyms/routes.ts
+++ src/controllers/gyms/routes.ts
@@ -1,11 +1,18 @@
 import { FastifyInstance } from 'fastify'
 import { verifyJwt } from '@/middlewares/verify-jwt'
 import { GymController } from './gymController'
+import { verifyUserRole } from '@/middlewares/verify-user-role'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt)
 
   app.get('/gyms/search', GymController.search)
   app.get('/gyms/nearby', GymController.nearby)
-  app.post('/gyms', GymController.register)
+  app.post(
+    '/gyms',
+    {
+      onRequest: verifyUserRole('ADMIN'),
+    },
+    GymController.register,
+  )
 }
