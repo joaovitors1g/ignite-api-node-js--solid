19d55a8e0491f10de8cb07ce8788f37d56224925
joaovitors1g <joao_vitor9846@hotmail.com>
2023-04-23 18:56:10 -0300

    test: add user admin creation to user auth test util

--- src/utils/test/create-and-authenticate-users.ts
+++ src/utils/test/create-and-authenticate-users.ts
@@ -1,11 +1,20 @@
+import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
 import { FastifyInstance } from 'fastify/types/instance'
 import request from 'supertest'
 
-export async function createAndAuthenticateUsers(app: FastifyInstance) {
-  await request(app.server).post('/users').send({
-    email: 'johndoe@example.com',
-    password: '12345678',
-    name: 'John Doe',
+export async function createAndAuthenticateUsers(
+  app: FastifyInstance,
+  isAdmin = false,
+) {
+  await prisma.user.deleteMany()
+  await prisma.user.create({
+    data: {
+      email: 'johndoe@example.com',
+      name: 'John Doe',
+      password_hash: await hash('12345678', 6),
+      role: isAdmin ? 'ADMIN' : 'MEMBER',
+    },
   })
 
   const authResponse = await request(app.server).post('/sessions').send({
