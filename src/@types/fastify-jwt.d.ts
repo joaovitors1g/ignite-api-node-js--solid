414e2504120b91008873164bca56f4269c10e157
joaovitors1g <joao_vitor9846@hotmail.com>
2023-04-23 18:16:10 -0300

    feat: add role to fastify user typing

--- src/@types/fastify-jwt.d.ts
+++ src/@types/fastify-jwt.d.ts
@@ -1,9 +1,11 @@
 import '@fastify/jwt'
+import { Role } from '@prisma/client'
 
 declare module '@fastify/jwt' {
   export interface FastifyJWT {
     user: {
       sub: string
+      role: (typeof Role)[keyof typeof Role]
     } // user type is return type of `request.user` object
   }
 }
