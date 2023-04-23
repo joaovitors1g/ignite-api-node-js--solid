e6ec0d707b4a6e73f55c7cdde9c4beb82829f238
joaovitors1g <joao_vitor9846@hotmail.com>
2023-04-23 18:01:06 -0300

    feat: add route for token refresh

--- src/controllers/users/routes.ts
+++ src/controllers/users/routes.ts
@@ -3,10 +3,12 @@ import { RegisterController } from './registerController'
 import { AuthController } from './authController'
 import { ProfileController } from './profileController'
 import { verifyJwt } from '@/middlewares/verify-jwt'
+import { TokenController } from './tokenController'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', RegisterController.register)
   app.post('/sessions', AuthController.authenticate)
+  app.patch('/token/refresh', TokenController.refresh)
 
   /** Authenticated Routes */
   app.get(
